8882c7ac7ea116d3e272a78f9489ecf75fefa738
shinelith <lith2008@gmail.com>
2019-07-15 16:03:41 +0800

    修改提示文字

--- Mods/tech/Moment/entry.lua
+++ Mods/tech/Moment/entry.lua
@@ -9,7 +9,7 @@ declare_plugin("Moment",
     displayName = "Moment",
     developerName = "LITH",
 	creditsFile = "credits.txt";
-    info = _("看腻了官方壁纸的话不如试试这个工具\n把Moment设为壁纸，每当启动DCS，主界面壁纸会自动随用户截图进行改变，一起回顾精彩时刻吧。"),
+    info = _("随机呈现截图作为壁纸，点击'设置壁纸'启用插件"),
 
     Skins	=
 	{
